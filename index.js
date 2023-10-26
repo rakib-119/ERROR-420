73c8253af3e31d6cdb6411f6734c2b9fe0034633
rakib-119 <145193097+rakib-119@users.noreply.github.com>
2023-10-26 12:50:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ say('ABHISHEK-SER', {
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`'${name}' By @ABHISHEK SURESH._`, {
+say(`'${name}' By @Err0r420._`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
